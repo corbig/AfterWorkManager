d482cb0a52b080f1907a709263f48c5834c39475
David <dada17xs@hotmail.com>
2017-01-13 14:29:41 +0100

    Indentation AWChat.js

--- src/AWChat.js
+++ src/AWChat.js
@@ -22,26 +22,24 @@ const style={
   marginLeft : 10
 }
 
-
 const toolbarStyle = {
-      backgroundColor: cyan500,
-      color : "#FFFFFF",
-      position: "relative",
-       marginTop:0,
-       zIndex: 2,
-       width:"100%"
+	backgroundColor: cyan500,
+	color : "#FFFFFF",
+	position: "relative",
+	marginTop:0,
+	zIndex: 2,
+	width:"100%"
 }
+
 const whiteStyle ={
   color : "#FFFFFF"
 }
 
 const cardStyle = {
-margin: 'auto',
-postion:"absolute",
-display: 'block',
-width:"100%",
-
-
+  margin: 'auto',
+  postion:"absolute",
+  display: 'block',
+  width:"100%",
 }
 
 const paperStyle={
@@ -51,65 +49,57 @@ const paperStyle={
 }
 
 
-
-
-
 export default class AWChat extends React.Component {
-
+  
   constructor(props) {
     super(props);
     this.state = {shadow: 1};
   }
-
-
-
+  
   render() {
     return (
       <Card style={cardStyle}>
-      <Toolbar style={toolbarStyle}>
-      <ToolbarGroup firstChild={true}>
-      <IconButton>
-       <CommunicationChatBubble color={"#FFFFFF"}/>
-      </IconButton>
-        <ToolbarTitle text="Conversation" style={whiteStyle}/>
-      </ToolbarGroup>
+        <Toolbar style={toolbarStyle}>
+          <ToolbarGroup firstChild={true}>
+            <IconButton>
+              <CommunicationChatBubble color={"#FFFFFF"}/>
+            </IconButton>
+            <ToolbarTitle text="Conversation" style={whiteStyle}/>
+          </ToolbarGroup>
 
-        <ToolbarGroup>
-
-        </ToolbarGroup>
-      </Toolbar>
-      <Paper style={paperStyle} zDepth={0}>
-      <List>
-     <ListItem
-       primaryText="Chelsea Otakan"
-       leftAvatar={<Avatar src="images/tux.png" />}
-     />
-     <Divider />
-     <ListItem
-       primaryText={<span style={{paddingRight:"10px"}}>Eric Hoffman</span>}
-       style={{textAlign:"right"}}
-       rightAvatar={<Avatar src="images/tux.png"/>}
-     />
-     <Divider />
-     <ListItem
-       primaryText="James Anderson"
-       secondaryText = "Salut !"
-       leftAvatar={<Avatar src="images/tux.png" />}
-     />
-     <Divider />
-     <ListItem
-       primaryText={<span style={{paddingRight:"10px"}}>Trucmuche</span>}
-       secondaryText = {<div style={{paddingRight:"10px"}}>Salut</div>}
-       style={{textAlign:"right"}}
-       rightAvatar={<Avatar src="images/tux.png"/>}
-     />
-
-   </List>
-   </Paper>
-  <AWInputCard/>
-    </Card>
-
-
+          <ToolbarGroup>
+          </ToolbarGroup>
+        </Toolbar>
+        
+        <Paper style={paperStyle} zDepth={0}>
+          <List>
+            <ListItem
+            primaryText="Chelsea Otakan"
+            leftAvatar={<Avatar src="images/tux.png" />}
+            />
+            <Divider />
+            <ListItem
+            primaryText={<span style={{paddingRight:"10px"}}>Eric Hoffman</span>}
+            style={{textAlign:"right"}}
+            rightAvatar={<Avatar src="images/tux.png"/>}
+            />
+            <Divider />
+            <ListItem
+            primaryText="James Anderson"
+            secondaryText = "Salut !"
+            leftAvatar={<Avatar src="images/tux.png" />}
+            />
+            <Divider />
+            <ListItem
+            primaryText={<span style={{paddingRight:"10px"}}>Trucmuche</span>}
+            secondaryText = {<div style={{paddingRight:"10px"}}>Salut</div>}
+            style={{textAlign:"right"}}
+            rightAvatar={<Avatar src="images/tux.png"/>}
+            />
+          </List>
+        </Paper>
+        <AWInputCard/>
+      </Card>
     );
   }
 }
